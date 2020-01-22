8a1020908a081f58b008e4705dd1d155a2d39c3b
nish2447 <57082077+nish2447@users.noreply.github.com>
2020-01-22 23:31:38 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,20 @@
+#! python3
+#print("Hello, python!")
+"""
+x='awesome'
+def fun():
+	global x
+	x='fantastic'
+	print('python is'+x)
+
+fun()
+print
+
+x='hi SIS'
+#y=int(x)
+print(x.lower())
+print(x.upper())
+"""
+for a in range(0,2):
+	tuple=('hi','hello','how r you')
+		print(tuple[-a])
